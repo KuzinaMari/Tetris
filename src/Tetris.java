d707ed95ec3bfb2f32e790d22ebc568cbfe4fb6b
KuzinaMari <Novamaria98@mail.ru>
2017-11-24 16:58:25 +0300

    Cделан алгоритм удаления рядов

--- src/Tetris.java
+++ src/Tetris.java
@@ -22,14 +22,59 @@ import java.util.ArrayList;
 
 public class Tetris {
     public static void main(String[] arg){
+        int hh = 1;
+        assert !Type.I.getFields()[0].canDraw( Type.O.getFields()[0], 0, 0 );
+        assert Type.I.getFields()[0].canDraw( Type.O.getFields()[0], 2, 0 );
+        assert !Type.I.getFields()[0].canDraw( Type.O.getFields()[0], -1, -1 );
+
+        Canvas c = new Canvas( 4, 5 );
+        byte[][] v3 = {
+                { 0, 1, 1, 0 },
+                { 0, 0, 1, 1 },
+                { 2, 1, 1, 1 },
+                { 2, 1, 1, 1 },
+                { 2, 1, 0, 0 },
+        };
+        c.set( v3 );
+        c.print();
+        boolean wasFull = false;
+        do {
+            wasFull = false;
+            for (int row = c.getHeight() - 1; row >= 0; row--) {
+                if (c.isFull(row)) {
+                    wasFull = true;
+                    for (int row2 = row - 1; row2 >= 0; row2--) {
+                        c.putDownRow(row2);
+                    }
+                    c.clearRow(0);
+                }
+            }
+        }while( wasFull );
+        c.print();
+
+        if( hh == 1 )
+            return;
+        c = new Canvas( 4, 3 );
+        byte[][] v = {
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 },
+                { 2, 0, 0, 0 },
+        };
+        c.set( v );
+        c.print();
+        c.draw( Type.O.getFields()[0], 2, 1 );
+        c.print();
+        c.putDownRow(1);
+        c.print();
+        if( hh == 1 )
+            return;
         for (Type t: Type.values()){
             System.out.println( t.name() );
-            for( Canvas c : t.getFields() ) {
+            for( Canvas c2 : t.getFields() ) {
                 System.out.println("");
-                c.print();
+                c2.print();
             }
         }
-        int hh = 1;
         if( hh == 1 )
             return;
 //        Area a = new Area (10, 20);
@@ -42,34 +87,78 @@ public class Tetris {
 //        Game game = new Game( pieces );
 //        int b = 5;
 
-        Canvas c = new Canvas( 4, 3 );
+        c = new Canvas( 4, 3 );
         c.put( 1, 0 ); c.put( 2, 0 );
         c.put( 2, 1 ); c.put( 2, 2 );
         c.print();
         System.out.println("");
         c = new Canvas( 4, 3 );
-        byte[][] v = {
+        byte[][] v2 = {
                 { 0, 1, 1, 0 },
                 { 0, 0, 1, 0 },
                 { 2, 0, 1, 0 },
         };
-        c.set( v );
+        c.set( v2 );
         c.print();
     }
+    //public static void assert()
 
 
     public static class  Area{
         private Canvas myField;
-        public Area( int height, int width ){
+        public Area( int width, int height ){
             myField = new Canvas( width, height );
         }
     }
 
     public static class Game{
         private ArrayList<Piece> myPieces;
+        private Move currentMove;
+        private Area area;
 
-        public Game(ArrayList<Piece> myPieces) {
-            this.myPieces = myPieces;
+        public Game(ArrayList<Piece> pieces, Move move) {
+            myPieces = pieces;
+            currentMove = move;
+            area = new Area( 5, 7 );
+        }
+
+        public void play(){
+            for( int time = 0; time < 20; time++ ){
+                
+            }
+        }
+    }
+
+    public static class Move{
+        private int myShift;
+        private int myRotation;
+        private int myTime;
+        private Move myNext;
+
+        public Move(int shift, int rotation, int time) {
+            myShift = shift;
+            myRotation = rotation;
+            myTime = time;
+        }
+
+        public int getShift() {
+            return myShift;
+        }
+
+        public int getRotation() {
+            return myRotation;
+        }
+
+        public int getTime() {
+            return myTime;
+        }
+
+        public Move getNext() {
+            return myNext;
+        }
+
+        public void setNext(Move myNext) {
+            this.myNext = myNext;
         }
     }
 
--- src/tetris/Canvas.java
+++ src/tetris/Canvas.java
@@ -46,4 +46,68 @@ public class Canvas {
         }
         System.out.println(res.toString());
     }
+
+    public boolean canDraw( Canvas c, int x, int y ){
+        for( int xi = 0; xi < c.myPixels.length; xi++ ){
+            for( int yi = 0; yi < c.myPixels[ xi ].length; yi++ ){
+                if( c.myPixels[ xi ][ yi ] != 0 ){
+                    int toX = x + xi;
+                    int toY = y + yi;
+                    if( ( toX <0 )||( toX >=getWidth() )||( toY <0 )||( toY >=getHeight() ) ){
+                        return false;
+                    }
+                    if( myPixels[ toX ][ toY ] != 0 ){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void draw( Canvas c, int x, int y ){
+        for( int xi = 0; xi < c.myPixels.length; xi++ ){
+            for( int yi = 0; yi < c.myPixels[ xi ].length; yi++ ){
+                if( c.myPixels[ xi ][ yi ] != 0 ){
+                    int toX = x + xi;
+                    int toY = y + yi;
+                    if( ( toX <0 )||( toX >=getWidth() )||( toY <0 )||( toY >=getHeight() ) ){
+                        throw new RuntimeException( "can't draw outside" );
+                    }
+                    if( myPixels[ toX ][ toY ] != 0 ){
+                        throw new RuntimeException( "pixel already exist" );
+                    }
+                    myPixels[ toX ][ toY ] = c.myPixels[ xi ][ yi ];
+                }
+            }
+        }
+    }
+
+    /*
+    Вопрос. если над строкой остаётся "кусок" ни с чем не связанный, то он же
+    должен "падать" вниз или он остаётся "подвешенным"?
+     */
+
+    //public boolean canDownRow()
+
+    public void putDownRow( int row ){
+        for( int xi = 0; xi < myPixels.length; xi++ ){
+            myPixels[ xi ][ row +1 ] = myPixels[ xi ][ row ];
+        }
+    }
+
+    public boolean isFull( int row ){
+        for( int xi = 0; xi < myPixels.length; xi++ ){
+            if( myPixels[ xi ][ row ] == 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void clearRow( int row ){
+        for( int xi = 0; xi < myPixels.length; xi++ ){
+            myPixels[ xi ][ row ] = 0;
+        }
+    }
 }
